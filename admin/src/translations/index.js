45ed659c6677405d7fa89c356c0fa1ecc1feca1c
peterhs.kang <peterhs.kang@gmail.com>
2021-05-30 00:28:17 +0900

    - custom admin

---
+++ admin/src/translations/index.js
@@ -0,0 +1,15 @@
+import en from './en.json';
+import ko from './ko.json';
+
+// With this, only English and Korean will be available in your admin
+const trads = {
+  en,
+  ko,
+};
+
+export const languageNativeNames = {
+  en: 'English',
+  ko: '한글',
+};
+
+export default trads;
