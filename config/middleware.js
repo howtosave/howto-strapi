b6e8db2d9f5705e0968e28575f2f993a69d8ea16
peterk <tiglek+u16office@gmail.com>
2021-10-18 20:29:34 +0900

    - fix prefix url issues

--- config/middleware.js
+++ config/middleware.js
@@ -1,7 +1,17 @@
-module.exports = {
+module.exports = ({env}) => ({
   settings: {
+    public: {
+      path: "./public",
+      defaultIndex: true,
+    },
+    favicon: {
+      path: "./public/favicon.ico",
+    },
+    router: {
+      prefix: env("URL_PREFIX", ""),
+    },
     cors: {
       headers: ['Content-Type', 'Authorization', 'Origin', 'Accept', 'Cache-Control'],
     },
   },
-};
+});
